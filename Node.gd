fd78f2f9b8014950a208dec4e105125550a3c847
Edouard Murat <edouardmurat1@gmail.com>
2020-06-30 09:11:06 -0400

    sunburst parts' mouseover works

--- Node.gd
+++ Node.gd
@@ -34,7 +34,7 @@ func _draw():
 		var p4 = center + Vector2(cos(end_angle), sin(end_angle)) * (level*radius+width/2)
 		draw_line(p3, p4, Color.white, 2.0)
 	
-#	build_collision_shape()
+	build_collision_shape()
 
 func add_node(label):
 	var node = node_scene.instance()
@@ -61,6 +61,7 @@ func update_children_angles():
 		child.start_angle = start_angle + i*(end_angle - start_angle) / nb_children
 		child.end_angle = (i+1) * end_angle / nb_children
 		child.color.g += 0.25*i
+		child.color_unselected.g += 0.25*i
 		child.update_children_angles()
 
 
@@ -69,30 +70,28 @@ func get_point_on_arc(radius, angle):
 
 func build_collision_shape():
 	var points = PoolVector2Array()
-	var inner_r = radius - width/2
-	var outer_r = radius + width/2
+	var inner_r = level*radius - width/2
+	var outer_r = level*radius + width/2
 	
-	points.append(get_point_on_arc(outer_r, start_angle))
-	points.append(get_point_on_arc(outer_r, start_angle + (end_angle-start_angle)/4.0))
-	points.append(get_point_on_arc(outer_r, start_angle + (end_angle-start_angle)/2.0))
-	points.append(get_point_on_arc(outer_r, end_angle - (end_angle-start_angle)/4.0))
-	points.append(get_point_on_arc(outer_r, end_angle))
+	for i in range(0, 9):
+		points.append(get_point_on_arc(outer_r, start_angle + i*(end_angle-start_angle)/8.0))
 	
-	points.append(get_point_on_arc(inner_r, end_angle))
-	points.append(get_point_on_arc(inner_r, end_angle - (end_angle-start_angle)/4.0))
-	points.append(get_point_on_arc(inner_r, start_angle + (end_angle-start_angle)/2.0))
-	points.append(get_point_on_arc(inner_r, start_angle + (end_angle-start_angle)/4.0))
-	points.append(get_point_on_arc(inner_r, start_angle))
+	for i in range(0, 9):
+		points.append(get_point_on_arc(inner_r, end_angle - i*(end_angle-start_angle)/8.0))
 	
-	for i in range(0, points.size()):
-		$Area2D/CollisionShape2D.shape.segments[i] = points[i]
+	$Area2D/CollisionPolygon2D.polygon = points
+	
+#	for i in range(0, points.size()):
+#		$Area2D/CollisionShape2D.shape.segments[i] = points[i]
 
 func _on_Area2D_mouse_entered():
+	print(color_unselected)
 	color = color_selected
 	update()
 #	$CircleSprite/CenterContainer/Label.set("custom_colors/font_color", Color(0,0,0))
 
 func _on_Area2D_mouse_exited():
+	print("exited")
 	color = color_unselected
 	update()
 #	$CircleSprite/CenterContainer/Label.set("custom_colors/font_color", Color(1,1,1))
